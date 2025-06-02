cc11741dd0dc7df7f3a672345e1e84136ac83615
shinn <zhouxy.vortex@gmail.com>
2025-06-03 00:07:53 +0800

    更新yazi的配置文件

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,3 +1,5 @@
 require("full-border"):setup()
 require("relative-motions"):setup({ show_numbers = "relative", show_motion = true, enter_mode = "first" })
 require("git"):setup()
+
+require("starship"):setup()
