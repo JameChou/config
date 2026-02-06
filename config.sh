815aaa1eb9a9eece30a932affc912b1416d17b87
Shinn <zhouxy.vortex@gmail.com>
2026-02-06 14:26:53 +0800

    更新yazi的配置以及kitty的配置，让其支持neovim

---
+++ config.sh
@@ -0,0 +1,27 @@
+!#/bin/bash
+export ALL_PROXY='http://127.0.0.1:7897'
+
+sudo pacman -S fzf wl-clipboard cliphist wf-recorder slurp ffmpeg zsh neovim tmux waybar
+
+cd ~/.config
+ln -s ~/Documents/config/hypr/ .
+ln -s ~/Documents/config/gtk-3.0/ .
+ln -s ~/Documents/config/gtk-4.0/ .
+ln -s ~/Documents/config/gtkrc-2.0 .
+ln -s ~/Documents/config/tmux/ .
+ln -s ~/Documents/config/rofi/ .
+ln -s ~/Documents/config/kitty/ .
+ln -s ~/Documents/config/mpv/ .
+ln -s ~/Documents/config/chrome-flags.conf .
+ln -s ~/Documents/config/fontconfig/ .
+ln -s ~/Documents/config/oh-my-posh/ .
+ln -s ~/Documents/config/waybar/ .
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/jeffreytse/zsh-vi-mode.git $ZSH_CUSTOM/plugins/zsh-vi-mode
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+curl -s https://ohmyposh.dev/install.sh | bash -s
+
+
